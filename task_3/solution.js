6625afe1a8cf2b35888623d7d015eaf4c51a224f
Roman Kharchenko <hrv2313@gmail.com>
2022-10-31 10:54:03 +0600

    task_3 done

--- task_3/solution.js
+++ task_3/solution.js
@@ -1,15 +1,17 @@
 function renderCartItem(item) {
     const product = item;
+    // Задание №3.1. Формирование строки корзины
     let itemCountText = '';
+
     itemCountText += product.count;
     itemCountText += ' × ';
     itemCountText += product.price;
     itemCountText += ' ₽ = ';
+    
     let sum = product.count * product.price;
+    
     itemCountText += sum;
     itemCountText += ' ₽';
-    // Задание №3.1. Формирование строки корзины
-
     // product - объект вида {id: id, title: title, count: count, price: price}
     // например, {id: 1, title: 'Пицца', count: 5, price: '500.00'}, где
     // id - идентификатор
